81f434602d0a60a13d05ca0020df6cc537b3a393
balao1312 <balao1014@hotmail.com>
2020-10-13 12:21:07 +0800

    Update #1290_convert_binary_number_in_a_linked_list_to_integer.py

--- #1290_convert_binary_number_in_a_linked_list_to_integer.py
+++ #1290_convert_binary_number_in_a_linked_list_to_integer.py
@@ -5,3 +5,12 @@ class Solution:
             num = num * 2 + head.next.val
             head = head.next
         return num
+
+
+    def getDecimalValue(self, head) -> int:
+        sum = 0
+        for ex in range(len(head) - 1, -1, -1):
+            print(len(head) - ex - 1)
+            print(2 ** ex)
+            sum += head[len(head) - 1 - ex] * (2 ** ex)
+        return sum
